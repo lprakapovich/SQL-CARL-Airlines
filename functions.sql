06a1ea969f6ce04790302b9ae49636dfa3b04b51
224097 Pekasiewicz Agnieszka <224097@edu.p.lodz.pl>
2021-01-03 17:37:04 +0100

    Added two functions for calculating sum of all discounts for a given reservation and final ticket price.

--- functions.sql
+++ functions.sql
@@ -26,4 +26,63 @@ RETURN v_reservations_number;
 END;
 
 
---
+--3) Calculate total discount for a given reservation.
+
+CREATE OR REPLACE FUNCTION calc_total_discount_percent 
+    (p_reservation_id   reservations.reservation_id%TYPE)
+RETURN NUMBER
+IS 
+    v_total_discount    discounts.discount_percent%TYPE;
+BEGIN
+    SELECT SUM(d.discount_percent)
+    INTO v_total_discount
+    FROM reservations r
+    LEFT JOIN discounts d
+    ON r.reservation_id = d.reservation_id
+    GROUP BY r.reservation_id
+    HAVING r.reservation_id = p_reservation_id;
+    
+    IF v_total_discount > 0 THEN
+        RETURN v_total_discount;
+    ELSE
+        RETURN 0;
+    END IF;
+END calc_total_discount_percent;
+
+
+
+	-- Testing
+
+SELECT reservation_id, calc_total_discount_percent(reservation_id) || '%' AS TOTAL_DISCOUNT
+    FROM reservations;
+
+
+    
+--4) Calculate final ticket price for a given reservation, after taking into account all discounts.
+
+
+CREATE OR REPLACE FUNCTION calc_ticket_price(
+    p_reservation_id   reservations.reservation_id%TYPE
+    )
+RETURN NUMBER
+IS
+    v_ticket_price      reservations.price%TYPE;
+    v_total_discount    discounts.discount_percent%TYPE; 
+BEGIN
+    SELECT price
+    INTO v_ticket_price
+    FROM reservations 
+    WHERE reservation_id = p_reservation_id;
+    
+    v_total_discount := calc_total_discount_percent(p_reservation_id);
+    
+    RETURN ROUND(v_ticket_price * (100 - v_total_discount) / 100, 2);  
+    
+END calc_ticket_price;
+
+
+
+	-- Testing
+
+SELECT reservation_id, price, calc_total_discount_percent(reservation_id) || '%' AS TOTAL_DISCOUNT, calc_ticket_price(reservation_id) AS TICKET_PRICE
+    FROM reservations;
